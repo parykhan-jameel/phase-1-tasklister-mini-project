d5cd466db19fc232f40773d005f9bd15893f667b
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-15 16:03:40 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createNewList = document.querySelector("input[type='submit']");
+  const input = document.querySelector("#new-task-description");
+  const list = document.querySelector("#tasks");
+  createNewList.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (input.value != "") {
+      const button = document.createElement("button");
+      button.textContent = "x";
+      const newTask = document.createElement("li");
+      newTask.textContent = input.value;
+      list.appendChild(newTask);
+      newTask.appendChild(button);
+      input.value = "";
+      button.addEventListener("click", () => newTask.remove());
+    } else {
+      alert("Value can't be empty, please enter a value");
+    }
+  });
 });
